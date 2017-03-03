4b91da1e1118d14f662e438e5bda8a7e00acc6c2
Ege Tumer <egetumer@gmail.com>
2017-03-03 09:05:15 -0400

    trivial change in App in order to attempt committing, pushing, and making this branch "branchtest" recognized by github.

--- email-validator/src/main/java/wilhelm/email_validator/App.java
+++ email-validator/src/main/java/wilhelm/email_validator/App.java
@@ -8,7 +8,7 @@ public class App
 {
 	public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        // System.out.println( "Hello World!" );
         
         String email0 = "user@domain.com";
         String email1 = "userdomain.com";
